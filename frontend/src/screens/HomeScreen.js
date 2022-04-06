beb9ee99ae18c2b107ce7dfd94dd2f16ab65d65e
Zinnia <zinnia2563@gmail.com>
2022-04-06 13:57:38 +0600

    manage state by Reducer Hook

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-function HomeScreen() {
-  const [products, setProducts] = useState([]);
-  useEffect(()=>{
-      const fetchData = async()=>{
-          const result =await axios.get('/api/products');
-          setProducts(result.data);
-      };
+import axios from "axios";
 
-      fetchData();
-      
-  },[]);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products:[],
+    loading: true,
+    error: '',
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+
+     // setProducts(result.data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
