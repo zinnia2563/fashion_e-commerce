6b862093ccb5ef88d3c510c4413b3a880302df28
Zinnia <zinnia2563@gmail.com>
2022-04-06 13:36:26 +0600

    fetch products from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,40 @@
-import { Link } from 'react-router-dom';
-import data from '../data';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 function HomeScreen() {
-    return (
-        <div>
-            <h1>Featured Products</h1>
-            <div className="products">
-                {data.products.map((product) => (
-                    <div className="product" key={product.slug}>
-                        <Link to={`/product/${product.slug}`}>
-                            <img src={product.image} alt={product.name}/>
-                        </Link>
-                        <div className="product-info">
-                            <Link to={`/product/${product.slug}`}>
-                                <p>{product.name}</p>
-                            </Link>
-                            <p>
-                                <strong>${product.price}</strong>
-                            </p>
-                            <button>Add to Cart</button>
-                        </div>
-                    </div>
-                ))}
+  const [products, setProducts] = useState([]);
+  useEffect(()=>{
+      const fetchData = async()=>{
+          const result =await axios.get('/api/products');
+          setProducts(result.data);
+      };
+
+      fetchData();
+      
+  },[]);
+
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to Cart</button>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default HomeScreen;
