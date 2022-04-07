f2fcb67ed4c81b297e7dfcbf20a08c3ee403a0d8
Zinnia <zinnia2563@gmail.com>
2022-04-07 08:01:14 +0600

    loading and messagebox from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,7 +7,9 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
 import { Badge, Button, Card } from 'react-bootstrap';
-
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -37,7 +39,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError (err)});
       }
     };
 
@@ -45,9 +47,11 @@ function ProductScreen() {
   }, [slug]);
 
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox/>
+ 
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
+   
   ) : (
     <div>
       <Row>
